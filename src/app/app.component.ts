0da28e1ed807e4811464f52098c0403eabac8211
Julien <julienbroens@gmail.com>
2020-07-16 14:32:46 +0200

    add unsubcripte

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AppareilService } from './services/appareil.service';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/Rx';
+import { Subscription } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   secondes: number;
+  counterSubscription: Subscription;
 
   constructor() {}
 
   ngOnInit() {
     const counter = Observable.interval(1000);
-    counter.subscribe(
-      (value: number) => {
-        this.secondes = value;
-      },
-      (error: any) => {
-        console.log('Une erreur a été rencontrée !');
-      },
-      () => {
-        console.log('Observable complété !');
-      }
-    );
+    this.counterSubscription = counter.subscribe((value: number) => {
+      this.secondes = value;
+    });
+  }
+
+  ngOnDestroy() {
+    this.counterSubscription.unsubscribe();
   }
 }
